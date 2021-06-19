09865941719129bb27c9d3d4c61744f5439bb2b8
SuruLove <srukeylove@gmail.com>
2021-06-19 22:22:13 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,32 @@
+pip install time
+pip install json
+pip install random
+pip install string
+pip install collections
+pip install urllib
+pip install bs4
+pip install colorama
+sleep 1
+clear
+cd SuruBruteForce
+chmod +x *
+cd ..
+python SuruBruteForce/install.py
+echo ''
+cd ig
+chmod +x *
+cd ..
+apt update
+apt upgrade
+pkg install python -y
+pkg install python2  -y
+pkg install figlet -y
+cd ig
+pip2 install mechanize
+pip install requests
+pip install -r requirements.txt
+cd ..
+echo ''
+sleep 1
+clear
+python SuruBruteForce/installing.py
